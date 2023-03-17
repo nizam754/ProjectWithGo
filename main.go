b06f06f52f9e714755d7b7729485aa2cb628e353
Mostafa Nizam <m.nizam754@gmail.com>
2023-03-18 01:29:09 +0600

    main.go fix

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 
 	http.HandleFunc("/", home)
+	http.HandleFunc("/features", features)
+	http.HandleFunc("/docs", docs)
 	http.Handle("/resources", http.StripPrefix("/resources", http.FileServer(http.Dir("/assets"))))
 	http.ListenAndServe(":8888", nil)
 }
@@ -16,6 +18,39 @@ func main() {
 func home(w http.ResponseWriter, r *http.Request) {
 
 	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func features(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func docs(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
